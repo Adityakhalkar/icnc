70ab1a23321f220102cef8b78825f734232fec15
Aditya Khalkar <adityakhalkar@Adityas-MacBook-Air.local>
2024-07-14 13:15:59 +0530

    Uploaded to pip

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
-# setup.py
+from setuptools import setup, find_packages
 
- from setuptools import setup, find_packages
-
- setup(
-     name="icnc",
-     version="0.1",
-     packages=find_packages(),
-     install_requires=[
-         "openai",
-         "python-dotenv",
-     ],
-     entry_points={
-         'console_scripts': [
-             'icnc=cli._cli:main',
-         ],
-     },
+setup(
+    name='icnc',  # Replace with your package name
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'icnc = icnc.main:main'  # Replace with your entry point
+        ]
+    },
+    install_requires=[
+        'python-dotenv',
+        'openai==0.12.6'  # Replace with your dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
